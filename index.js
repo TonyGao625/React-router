828c08b9af84cfedec6e5379769c3bf1ca850903
tony <784627606@qq.com>
2017-04-19 23:37:46 +0800

    set multiply pages

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Router, Route, hashHistory ,IndexRoute} from 'react-router'
+import About from './modules/About'
+import Repos from './modules/Repos'
+import Home from './modules/Home'
 import App from './modules/App'
-render(<App/>, document.getElementById('app'))
+render(
+    <Router history={hashHistory}>
+        <Route path="/" component={App}>
+        <IndexRoute component={Home}/>
+            {/* make them children of `App` */}
+            <Route path="/repos" component={Repos} />
+            <Route path="/about" component={About} />
+        </Route>
+    </Router>
+    , document.getElementById('app'))
